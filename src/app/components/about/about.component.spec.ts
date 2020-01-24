261578f9177fd8146b005c356c10181548e4d471
Csete Gábor <csgabor88@gmail.com>
2020-01-24 15:11:17 +0100

    about-nocss + .secret

--- src/app/components/about/about.component.spec.ts
+++ src/app/components/about/about.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MapmockComponent } from './mapmock.component';
+import { AboutComponent } from './about.component';
 
-describe('MapmockComponent', () => {
-  let component: MapmockComponent;
-  let fixture: ComponentFixture<MapmockComponent>;
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapmockComponent ]
+      declarations: [ AboutComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MapmockComponent);
+    fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,15 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { Secret } from './../../.secret';
+
+const secret = new Secret();
 
 export const environment = {
-  agmApiKey: '',
-  hostname: 'http://localhost:3000',
+  agmApiKey: secret.apikey,
+  hostname: secret.hostname,
   production: false
 };
-
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
